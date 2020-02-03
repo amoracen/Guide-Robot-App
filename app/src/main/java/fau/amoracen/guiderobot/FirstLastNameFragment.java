e9b90d522db93f9b96aa8ce048f3d43acb3bdc48
Álejandro Moracen <amoracen93@gmail.com>
2020-02-03 01:44:25 -0500

    Using getArguments to retrieve data

--- app/src/main/java/fau/amoracen/guiderobot/FirstLastNameFragment.java
+++ app/src/main/java/fau/amoracen/guiderobot/FirstLastNameFragment.java
@@ -1,18 +1,16 @@
 package fau.amoracen.guiderobot;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
 
 
 public class FirstLastNameFragment extends Fragment {
@@ -26,6 +24,8 @@ public class FirstLastNameFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull final View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        Toast.makeText(getContext(),getArguments().getString("email"),Toast.LENGTH_SHORT).show();
 
         Button buttonNextPage = view.findViewById(R.id.nextPageButton);
         buttonNextPage.setOnClickListener(new View.OnClickListener() {
