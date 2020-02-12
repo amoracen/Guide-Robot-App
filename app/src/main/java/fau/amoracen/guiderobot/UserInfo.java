118fe123cca6e9cacab8dedee1c69c45fd1a4f0a
Álejandro Moracen <amoracen93@gmail.com>
2020-02-11 23:55:49 -0500

    Initial Commit

---
+++ app/src/main/java/fau/amoracen/guiderobot/UserInfo.java
@@ -0,0 +1,60 @@
+package fau.amoracen.guiderobot;
+
+/**
+ * Class to represent the user's information
+ */
+public class UserInfo {
+    private String firstName;
+    private String lastName;
+    private String heightFeet;
+    private String heightInches;
+
+    /**
+     * Default Constructor
+     */
+    public UserInfo() {
+
+    }
+
+    /**
+     * Parameterized constructor
+     */
+    public UserInfo(String firstName, String lastName, String heightFeet, String heightInches) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.heightFeet = heightFeet;
+        this.heightInches = heightInches;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getHeightFeet() {
+        return heightFeet;
+    }
+
+    public void setHeightFeet(String heightFeet) {
+        this.heightFeet = heightFeet;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getHeightInches() {
+        return heightInches;
+    }
+
+    public void setHeightInches(String heightInches) {
+        this.heightInches = heightInches;
+    }
+}
