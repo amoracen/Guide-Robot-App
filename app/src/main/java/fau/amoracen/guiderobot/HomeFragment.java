05397e68f75deefefa89f903198708b9fb806f51
Álejandro Moracen <amoracen93@gmail.com>
2020-03-18 17:07:12 -0400

    Accessibility event added

--- app/src/main/java/fau/amoracen/guiderobot/HomeFragment.java
+++ app/src/main/java/fau/amoracen/guiderobot/HomeFragment.java
@@ -1,15 +1,10 @@
 package fau.amoracen.guiderobot;
 
 
-import android.content.Context;
 import android.os.Bundle;
-import android.os.CountDownTimer;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.accessibility.AccessibilityEvent;
-import android.view.accessibility.AccessibilityManager;
 import android.widget.Button;
 
 import androidx.annotation.NonNull;
@@ -21,8 +16,6 @@ import androidx.navigation.Navigation;
  * HomeFragment displays the button to Create Account and Login
  */
 public class HomeFragment extends Fragment {
-
-    private boolean buttonPressed = false;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
@@ -39,7 +32,6 @@ public class HomeFragment extends Fragment {
         buttonRegistration.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                buttonPressed = true;
                 Navigation.findNavController(view).navigate(R.id.action_homeFragment_to_regitrationFragment, null);
             }
         });
@@ -48,7 +40,6 @@ public class HomeFragment extends Fragment {
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                buttonPressed = true;
                 Navigation.findNavController(view).navigate(R.id.action_homeFragment_to_loginFragment, null);
             }
         });
@@ -57,28 +48,7 @@ public class HomeFragment extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
-        if (buttonPressed) {
-            buttonPressed = false;
-            //Wait
-            new CountDownTimer(400, 1000) {
-                public void onTick(long millisecondsUntilDone) {
-                    //countdown every second
-                }
-
-                public void onFinish() {
-                    //Counter is finished
-                    Log.i("Done ", "CountDown Finished");
-                    AccessibilityManager manager = (AccessibilityManager) getContext().getSystemService(Context.ACCESSIBILITY_SERVICE);
-                    if (manager != null && manager.isEnabled()) {
-                        AccessibilityEvent e = AccessibilityEvent.obtain();
-                        e.setEventType(AccessibilityEvent.TYPE_ANNOUNCEMENT);
-                        e.setClassName(getClass().getName());
-                        e.setPackageName(getContext().getPackageName());
-                        e.getText().add(getString(R.string.welcome_msg));
-                        manager.sendAccessibilityEvent(e);
-                    }
-                }
-            }.start();
-        }
+        /*Start an Accessibility Event*/
+        MyAccessibilityEvent.getInstance().sendAccessibilityEvent(getContext(), getString(R.string.welcome_msg));
     }
 }
