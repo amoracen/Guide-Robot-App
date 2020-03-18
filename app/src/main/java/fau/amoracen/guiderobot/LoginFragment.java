04edb69be84606e32ad1ea4cc25cb827b64ebe04
Álejandro Moracen <amoracen93@gmail.com>
2020-03-18 17:14:31 -0400

    new features Accessibility event and Speech to Text

--- app/src/main/java/fau/amoracen/guiderobot/LoginFragment.java
+++ app/src/main/java/fau/amoracen/guiderobot/LoginFragment.java
@@ -1,36 +1,32 @@
 package fau.amoracen.guiderobot;
 
-import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.CountDownTimer;
+import android.speech.RecognitionListener;
+import android.speech.SpeechRecognizer;
 import android.util.Log;
-import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.accessibility.AccessibilityEvent;
-import android.view.accessibility.AccessibilityManager;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.Navigation;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.FirebaseNetworkException;
 import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 import com.google.firebase.auth.FirebaseAuthInvalidUserException;
 
-import java.util.regex.Pattern;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Objects;
 
 /**
  * Login Fragment. Email and Password validation
@@ -41,7 +37,10 @@ public class LoginFragment extends Fragment {
     private TextInputLayout passwordLayout;
     private String emailInput;
     private String passwordInput;
-    private FirebaseAuth fireBaseAuth;
+    //Firebase
+    private Firebase myFirebase;
+    private FloatingActionButton micButton;
+    private SpeechToTextService speechToTextService;
 
 
     @Override
@@ -55,26 +54,33 @@ public class LoginFragment extends Fragment {
     public void onViewCreated(@NonNull final View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         //Firebase
-        fireBaseAuth = FirebaseAuth.getInstance();
+        myFirebase = new Firebase();
+        //Microphone
+        micButton = view.findViewById(R.id.micButton);
+        micButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                speechToTextService.startListening(getActivity());
+            }
+        });
+        initializeSpeechRecognizer();
 
         Button buttonNextPage = view.findViewById(R.id.loginButton);
         buttonNextPage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (!validateEmail()) {
-                    //Show Error
-                    return;
-                }
-                if (validatePassword()) {
-                    fireBaseAuth.signInWithEmailAndPassword(emailInput, passwordInput).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                if (validateEmail() && validatePassword()) {
+                    myFirebase.getMyFirebaseAuth().signInWithEmailAndPassword(emailInput, passwordInput).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if (task.isSuccessful()) {
-                                //Go to next Page
-                                Navigation.findNavController(view).navigate(R.id.action_LoginFragment_to_DashboardActivity, null);
+                                //Go to Dashboard
+                                Intent goToDashboard = new Intent(getContext(), DashboardActivity.class);
+                                startActivity(goToDashboard);
+                                Objects.requireNonNull(getActivity()).finishAffinity();
                             } else {
                                 try {
-                                    throw task.getException();
+                                    throw Objects.requireNonNull(task.getException());
                                 } catch (FirebaseAuthInvalidUserException e) {
                                     emailInputLayout.setError("Invalid Email");
                                     emailInputLayout.requestFocus();
@@ -84,7 +90,7 @@ public class LoginFragment extends Fragment {
                                 } catch (FirebaseNetworkException e) {
                                     Toast.makeText(getContext(), "Failed to login.No Network available", Toast.LENGTH_LONG).show();
                                 } catch (Exception e) {
-                                    Log.e("Failed", e.getMessage());
+                                    Log.e("Failed", Objects.requireNonNull(e.getMessage()));
                                 }
                                 Log.w("Failed", "signInWithEmail:failed", task.getException());
                             }
@@ -97,19 +103,89 @@ public class LoginFragment extends Fragment {
         });
         /*Email*/
         emailInputLayout = view.findViewById(R.id.emailTextInputLayout);
-        TextInputEditText emailInputEditText = view.findViewById(R.id.emailInputEditText);
         /*Password*/
         passwordLayout = view.findViewById(R.id.passwordTextInputLayout);
-        TextInputEditText passwordEditText = view.findViewById(R.id.passwordInputEditText);
+    }
 
-        TextView needHelpTextView = view.findViewById(R.id.needHelpTextView);
-        /*TODO*/
-        needHelpTextView.setOnClickListener(new View.OnClickListener() {
+    /**
+     * Initialize the Speech Recognition Listener
+     */
+    private void initializeSpeechRecognizer() {
+        speechToTextService = new SpeechToTextService(getContext());
+        if (!speechToTextService.isRecognitionAvailable()) {
+            micButton.setImageResource(R.drawable.ic_mic_off_blue_75dp);
+            micButton.setContentDescription(getString(R.string.mic_description_email_off));
+            micButton.setEnabled(false);
+            return;
+        }
+        speechToTextService.getSpeechRecognizer().setRecognitionListener(new RecognitionListener() {
             @Override
-            public void onClick(View view) {
-                sendAccessibilityEvent("Not Ready");
+            public void onReadyForSpeech(Bundle params) {
+            }
+
+            @Override
+            public void onBeginningOfSpeech() {
+
+            }
+
+            @Override
+            public void onRmsChanged(float rmsdB) {
+
+            }
+
+            @Override
+            public void onBufferReceived(byte[] buffer) {
+
+            }
+
+            @Override
+            public void onEndOfSpeech() {
+
+            }
+
+            @Override
+            public void onError(int error) {
+
+            }
+
+            @Override
+            public void onResults(Bundle results) {
+                List<String> result_arr = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+                assert result_arr != null;
+                processEmail(result_arr.get(0));
+            }
+
+            @Override
+            public void onPartialResults(Bundle partialResults) {
+
+            }
+
+            @Override
+            public void onEvent(int eventType, Bundle params) {
+
             }
         });
+    }
+
+    /**
+     * Cleaning the input before validation
+     *
+     * @param input a string
+     */
+    private void processEmail(String input) {
+        boolean atFound = false;
+        String[] temp = input.split(" ");
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < temp.length; i++) {
+            Log.i("INPUT", temp[i]);
+            if (!atFound && temp[i].contains("at")) {
+                temp[i] = "@";
+                atFound = true;
+            }
+            result.append(temp[i].toLowerCase().trim());
+        }
+        Objects.requireNonNull(emailInputLayout.getEditText()).setText(result);
+        validateEmail();
     }
 
     /**
@@ -118,18 +194,15 @@ public class LoginFragment extends Fragment {
      * @return true if a valid email was entered, false otherwise
      */
     private boolean validateEmail() {
-        emailInput = emailInputLayout.getEditText().getText().toString().trim();
-        emailInputLayout.setErrorEnabled(true);
-        if (emailInput.isEmpty()) {
-            emailInputLayout.setError("Field for email can't be empty");
+        emailInput = Objects.requireNonNull(emailInputLayout.getEditText()).getText().toString().trim();
+        HashMap hmap = ValidateInput.getInstance().validateEmail(getContext(), emailInput);
+        if (!Objects.equals(hmap.get("message"), "Valid")) {
+            emailInputLayout.setErrorEnabled(true);
+            emailInputLayout.setError(Objects.requireNonNull(hmap.get("message")).toString());
             return false;
-        } else if (!Patterns.EMAIL_ADDRESS.matcher(emailInput).matches()) {
-            emailInputLayout.setError("Please enter a valid email address");
-            return false;
-        } else {
-            emailInputLayout.setErrorEnabled(false);
-            return true;
         }
+        emailInputLayout.setErrorEnabled(false);
+        return true;
     }
 
     /**
@@ -137,84 +210,23 @@ public class LoginFragment extends Fragment {
      *
      * @return true if password is valid
      */
-    public boolean validatePassword() {
-        Pattern anyLetter = Pattern.compile("(?=.*[a-zA-Z])");
-        Pattern oneDigit = Pattern.compile("(?=.*[0-9])");
-        Pattern noWhiteSpace = Pattern.compile("\\s");
-        Pattern totalCharacters = Pattern.compile(".{6,}");
-
-        /*Checking Password*/
-        passwordInput = passwordLayout.getEditText().getText().toString().trim();
-        passwordLayout.setErrorEnabled(true);
-        if (passwordInput.isEmpty()) {
-            passwordLayout.setError("Field for password can't be empty");
-            return false;
-        } else if (!anyLetter.matcher(passwordInput).find()) {
-            passwordLayout.setError("Password must include a letter");
-            return false;
-        } else if (!oneDigit.matcher(passwordInput).find()) {
-            passwordLayout.setError("Password must include at least one digit");
-            return false;
-        } else if (noWhiteSpace.matcher(passwordInput).find()) {
-            passwordLayout.setError("No white spaces allowed");
-            return false;
-        } else if (!totalCharacters.matcher(passwordInput).find()) {
-            passwordLayout.setError("Password must be at least 6 characters long");
+    private boolean validatePassword() {
+        passwordInput = Objects.requireNonNull(passwordLayout.getEditText()).getText().toString().trim();
+        HashMap hmap = ValidateInput.getInstance().validatePassword(getContext(), passwordInput);
+        if (!Objects.equals(hmap.get("message"), "Valid")) {
+            passwordLayout.setErrorEnabled(true);
+            passwordLayout.setError(Objects.requireNonNull(hmap.get("message")).toString());
             return false;
         }
         passwordLayout.setErrorEnabled(false);
         return true;
     }
 
-    /**
-     * Hide Keyboard form View
-     *
-     * @param view current View
-     */
-    private void hideKeyboard(View view) {
-        try {
-            InputMethodManager inputMethodManager = (InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
-            inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * Wait for Voice Assistance and send an accessibility event
-     *
-     * @param msg a string
-     */
-    public void sendAccessibilityEvent(final String msg) {
-        //Wait
-        new CountDownTimer(400, 1000) {
-            public void onTick(long millisecondsUntilDone) {
-                //countdown every second
-            }
-
-            public void onFinish() {
-                //Counter is finished
-                Log.i("Done ", "CountDown Finished");
-                try {
-                    AccessibilityManager manager = (AccessibilityManager) getContext().getSystemService(Context.ACCESSIBILITY_SERVICE);
-                    if (manager != null && manager.isEnabled()) {
-                        AccessibilityEvent e = AccessibilityEvent.obtain();
-                        e.setEventType(AccessibilityEvent.TYPE_ANNOUNCEMENT);
-                        e.setClassName(getClass().getName());
-                        e.setPackageName(getContext().getPackageName());
-                        e.getText().add(msg);
-                        manager.sendAccessibilityEvent(e);
-                    }
-                } catch (NullPointerException e) {
-                    e.printStackTrace();
-                }
-            }
-        }.start();
-    }
 
     @Override
     public void onResume() {
         super.onResume();
-        sendAccessibilityEvent("Login Screen");
+        /*Start an Accessibility Event*/
+        MyAccessibilityEvent.getInstance().sendAccessibilityEvent(getContext(), "Login Screen");
     }
 }
