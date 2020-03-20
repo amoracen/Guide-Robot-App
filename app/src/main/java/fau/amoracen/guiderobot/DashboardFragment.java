65e97abd647719110c707c6ffe1a8d0488724b89
Álejandro Moracen <amoracen93@gmail.com>
2020-03-20 19:05:59 -0400

    refactor bluetooth check and added microphone

--- app/src/main/java/fau/amoracen/guiderobot/DashboardFragment.java
+++ app/src/main/java/fau/amoracen/guiderobot/DashboardFragment.java
@@ -6,12 +6,13 @@ import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.speech.RecognitionListener;
+import android.speech.SpeechRecognizer;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,24 +21,25 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import java.io.IOException;
-import java.util.Set;
+import java.util.List;
 
 /**
  * Dashboard Fragment
  */
 public class DashboardFragment extends Fragment {
 
-    private BluetoothAdapter bluetoothAdapter;
-    private String deviceName;
-    private String deviceAddress;
+    private static final int REQUEST_ENABLE_BLUETOOTH_Fragment = 2;
+    /*UI Widgets*/
     private TextView bluetoothTextView;
     private ImageView bluetoothImageView;
-    private EditText commandEditText;
     private TextView feedbackTextView;
-    private static final int REQUEST_ENABLE_BLUETOOTH = 11;
-    private Button sendCommandButton;
+    /*Bluetooth*/
     private BluetoothConnection myConnection;
+    private BluetoothDevice myDevice;
+    private BluetoothState bluetoothState;
     private boolean destroyed;
     private boolean connected = false;
     private String messageToSend;
@@ -45,6 +47,9 @@ public class DashboardFragment extends Fragment {
     private static final String SUCCESS_MSG = "MESSAGE_SENT";
     private static final String FAILED = "NOT CONNECTED";
     private static final String FAILED_MSG = "MESSAGE_FAILED";
+    private static final String BluetoothEnable = "BluetoothEnable";
+    private FloatingActionButton micButton;
+    private SpeechToTextService speechToTextService;
 
 
     @Nullable
@@ -60,81 +65,169 @@ public class DashboardFragment extends Fragment {
 
         bluetoothImageView = view.findViewById(R.id.bluetoothLogo);
         bluetoothTextView = view.findViewById(R.id.bluetoothTextView);
-        commandEditText = view.findViewById(R.id.commandEditText);
-        sendCommandButton = view.findViewById(R.id.sendButton);
         feedbackTextView = view.findViewById(R.id.feedback_text_view);
-        Button startConnection = view.findViewById(R.id.startConnectionButton);
-        Button goForwardBTN = view.findViewById(R.id.goForwardButton);
-        Button goInCirclesBTN = view.findViewById(R.id.goInCirclesButton);
-        Button drawSquareBTN = view.findViewById(R.id.drawSquareButton);
         destroyed = false;
         //Get bluetooth adapter
-        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        bluetoothState = BluetoothState.getInstance(BluetoothAdapter.getDefaultAdapter());
         checkBluetoothState();
 
-        startConnection.setOnClickListener(new View.OnClickListener() {
+        Button doorButton = view.findViewById(R.id.doorButton);
+        doorButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startConnection startConnection = new startConnection();
-                startConnection.execute();
+                setConnection("OPTION1");
             }
         });
-
-        sendCommandButton.setOnClickListener(new View.OnClickListener() {
+        Button option2Button = view.findViewById(R.id.option2Button);
+        option2Button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                messageToSend = commandEditText.getText().toString();
-                if (messageToSend.isEmpty()) {
-                    Toast.makeText(getContext(), "Please enter a Command", Toast.LENGTH_LONG).show();
-                    commandEditText.requestFocus();
-                } else {
-                    sendMessage sendMessage = new sendMessage();
-                    sendMessage.execute();
-                }
+                setConnection("OPTION2");
             }
         });
-        goForwardBTN.setOnClickListener(new View.OnClickListener() {
+        Button option3Button = view.findViewById(R.id.option3Button);
+        option3Button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                messageToSend = "GO_FORWARD";
-                sendMessage sendMessage = new sendMessage();
-                sendMessage.execute();
+                setConnection("OPTION3");
             }
         });
-        goInCirclesBTN.setOnClickListener(new View.OnClickListener() {
+        Button shutdownBTN = view.findViewById(R.id.shutdownButton);
+        shutdownBTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                messageToSend = "GO_IN_CIRCLES";
-                sendMessage sendMessage = new sendMessage();
-                sendMessage.execute();
+                setConnection("STOP");
             }
         });
-        drawSquareBTN.setOnClickListener(new View.OnClickListener() {
+        micButton = view.findViewById(R.id.micButton);
+        micButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                messageToSend = "DRAW_SQUARE";
-                sendMessage sendMessage = new sendMessage();
-                sendMessage.execute();
+                speechToTextService.startListening(getActivity());
             }
         });
-
+        initializeSpeechRecognizer();
     }//EOF onViewCreated
+
+    /**
+     * Initialize the Speech Recognition Listener
+     */
+    private void initializeSpeechRecognizer() {
+        speechToTextService = new SpeechToTextService(getContext());
+        if (!speechToTextService.isRecognitionAvailable()) {
+            micButton.setImageResource(R.drawable.ic_mic_off_blue_75dp);
+            micButton.setContentDescription(getString(R.string.mic_description_dashboard));
+            micButton.setEnabled(false);
+            return;
+        }
+        speechToTextService.getSpeechRecognizer().setRecognitionListener(new RecognitionListener() {
+            @Override
+            public void onReadyForSpeech(Bundle params) {
+            }
+
+            @Override
+            public void onBeginningOfSpeech() {
+
+            }
+
+            @Override
+            public void onRmsChanged(float rmsdB) {
+
+            }
+
+            @Override
+            public void onBufferReceived(byte[] buffer) {
+
+            }
+
+            @Override
+            public void onEndOfSpeech() {
+
+            }
+
+            @Override
+            public void onError(int error) {
+
+            }
+
+            @Override
+            public void onResults(Bundle results) {
+                List<String> result_arr = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+                assert result_arr != null;
+                processInput(result_arr.get(0).toLowerCase());
+            }
+
+            @Override
+            public void onPartialResults(Bundle partialResults) {
+
+            }
+
+            @Override
+            public void onEvent(int eventType, Bundle params) {
+
+            }
+        });
+    }
+
+    /**
+     * Create a connection to the server using the user's input
+     *
+     * @param input a string
+     */
+    private void processInput(String input) {
+        String search = "take";
+        if (input.contains(search)) {
+            if (input.contains("door")) {
+                createToast("Take me to: -> " + input);
+                setConnection("OPTION1");
+            } else if (input.contains("table")) {
+                createToast("Take me to: -> " + input);
+                setConnection("OPTION2");
+            } else if (input.contains("restroom")) {
+                createToast("Take me to: -> " + input);
+                setConnection("OPTION3");
+            } else {
+                createToast(input);
+            }
+        } else {
+            if (input.contains("shut down")) {
+                createToast("Take me to: -> " + input);
+                setConnection("STOP");
+            } else {
+                createToast(input);
+            }
+        }
+    }
+
+    /**
+     * Start Intent to request permission to use Bluetooth
+     */
+    private void startBluetoothIntent() {
+        Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+        startActivityForResult(enableIntent, REQUEST_ENABLE_BLUETOOTH_Fragment);
+    }
+
+    /**
+     * Create a Toast message
+     *
+     * @param message a string
+     */
+    private void createToast(String message) {
+        Toast.makeText(getContext(), message, Toast.LENGTH_LONG).show();
+    }
 
     /**
      * Checking if Bluetooth is Enabled
      */
     private void checkBluetoothState() {
-        if (bluetoothAdapter == null) {
-            Toast.makeText(getContext(), "Bluetooth is not supported on your device!", Toast.LENGTH_SHORT).show();
+        if (!bluetoothState.BluetoothSupported()) {
+            createToast("Bluetooth is not supported on your device!");
         } else {
-            if (!bluetoothAdapter.isEnabled()) {
-                //Toast.makeText(getActivity(), "You Need to Enable Bluetooth", Toast.LENGTH_SHORT).show();
-                Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-                startActivityForResult(enableIntent, REQUEST_ENABLE_BLUETOOTH);
-            } else {
-                bluetoothImageView.setImageResource(R.drawable.ic_bluetooth_enabled);
-                bluetoothImageView.setContentDescription("Bluetooth is Enable");
+            if (bluetoothState.checkBluetoothState()) {
+                updateUIButtons(BluetoothEnable);
                 checkPairedDevices();
+            } else {
+                startBluetoothIntent();
             }
         }
     }
@@ -149,48 +242,46 @@ public class DashboardFragment extends Fragment {
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == Activity.RESULT_OK) {
-            Toast.makeText(getContext(), "Bluetooth is On", Toast.LENGTH_LONG).show();
-            bluetoothImageView.setImageResource(R.drawable.ic_bluetooth_enabled);
-            bluetoothImageView.setContentDescription("Bluetooth is Enable");
-            checkPairedDevices();
-        }
-        if (resultCode == Activity.RESULT_CANCELED) {
-            Toast.makeText(getContext(), "Bluetooth is Off", Toast.LENGTH_LONG).show();
-            sendCommandButton.setEnabled(false);
+        if (requestCode == REQUEST_ENABLE_BLUETOOTH_Fragment) {
+            if (resultCode == Activity.RESULT_OK) {
+                updateUIButtons(BluetoothEnable);
+                checkPairedDevices();
+            }
+            if (resultCode == Activity.RESULT_CANCELED) {
+                Toast.makeText(getContext(), "Bluetooth is not Enable", Toast.LENGTH_LONG).show();
+                /*TODO DISABLE ALL BUTTONS*/
+            }
         }
     }
 
     /**
      * Checking if the devices were paired
      */
-    public void checkPairedDevices() {
-        //Check paired devices
-        Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
-        boolean deviceFound = false;
-        if (pairedDevices.size() > 0) {
-            // There are paired devices. Get the name and address of each paired device.
-            for (BluetoothDevice device : pairedDevices) {
-                Log.i("Devices Paired", device.getName());
-                if ("guide-robot".equals(device.getName())) {
-                    deviceName = device.getName();
-                    deviceAddress = device.getAddress();
-                    deviceFound = true;
-                }
-            }
-        }
-        if (deviceFound) {
+    private void checkPairedDevices() {
+        myDevice = bluetoothState.getBluetoothDevice();
+        if (myDevice != null) {
+            String deviceName = myDevice.getName();
+            //String deviceAddress = myDevice.getAddress();
             bluetoothTextView.setText(deviceName.toUpperCase());
             bluetoothTextView.setContentDescription("Pair to " + deviceName);
             //Check Connection on the background
-            startConnection startConnection = new startConnection();
-            startConnection.execute();
+            setConnection(null);
         } else {
-            Toast.makeText(getContext(), "There are not paired devices", Toast.LENGTH_SHORT).show();
-            sendCommandButton.setEnabled(false);
+            createToast("There are not paired devices");
+            /*TODO DISABLE ALL BUTTONS*/
         }
     }
 
+    /**
+     * Prepare message to Send and start Connection to Server
+     *
+     * @param msg a string
+     */
+    private void setConnection(String msg) {
+        startConnection startConnection = new startConnection();
+        messageToSend = msg;
+        startConnection.execute();
+    }
 
     /**
      * Start Connection to server
@@ -200,9 +291,12 @@ public class DashboardFragment extends Fragment {
         protected Void doInBackground(Void... params) {
             try {
                 /*Start a new Connection*/
-                final BluetoothDevice device = bluetoothAdapter.getRemoteDevice(deviceAddress);
-                myConnection = new BluetoothConnection(device);
+                myConnection = new BluetoothConnection(myDevice);
                 myConnection.start();
+                if (messageToSend != null) {
+                    //Send a message
+                    myConnection.send(messageToSend);
+                }
                 connected = true;
             } catch (IOException e) {
                 e.printStackTrace();
@@ -216,63 +310,50 @@ public class DashboardFragment extends Fragment {
         protected void onPostExecute(Void aVoid) {
             super.onPostExecute(aVoid);
             if (connected) {
-                updateUIMessage(SUCCESS);
+                if (messageToSend == null) {
+                    updateUIMessage(SUCCESS);
+                } else {
+                    updateUIMessage(SUCCESS_MSG);
+                }
                 myConnection.closeSocket();
             } else {
-                updateUIMessage(FAILED);
+                if (messageToSend == null) {
+                    updateUIMessage(FAILED);
+                } else {
+                    updateUIMessage(FAILED_MSG);
+                }
             }
         }
     }
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        destroyed = true;
-        if (myConnection != null) {
-            myConnection.closeSocket();
-        }
-    }
 
     /**
-     * Start Connection to server
+     * Update UI buttons
+     * Enable buttons if bluetooth is enable
+     * Disable buttons if bluetooth is not enable
+     *
+     * @param type a string
      */
-    public class sendMessage extends AsyncTask<Void, Void, Void> {
-        @Override
-        protected Void doInBackground(Void... params) {
-            try {
-                /*Start a new Connection*/
-                final BluetoothDevice device = bluetoothAdapter.getRemoteDevice(deviceAddress);
-                myConnection = new BluetoothConnection(device);
-                myConnection.start();
-                myConnection.send(messageToSend);
-                connected = true;
-            } catch (IOException e) {
-                e.printStackTrace();
-                Log.i("Exception", "Failed to Connect");
-                connected = false;
-            }
-            return null;
+    private void updateUIButtons(String type) {
+        //Check if user change fragments
+        if (destroyed) {
+            return;
         }
-
-        @Override
-        protected void onPostExecute(Void aVoid) {
-            super.onPostExecute(aVoid);
-            if (connected) {
-                updateUIMessage(SUCCESS_MSG);
-                myConnection.closeSocket();
-            } else {
-                updateUIMessage(FAILED_MSG);
-            }
-
+        switch (type) {
+            case BluetoothEnable:
+                bluetoothImageView.setImageResource(R.drawable.ic_bluetooth_enabled);
+                bluetoothImageView.setContentDescription("Bluetooth is Enable");
+                createToast("Bluetooth is Enable");
+                break;
         }
     }
 
     /**
-     * Update UI after AsyncTask is executed
+     * Update UI Message after AsyncTask is executed
      *
      * @param connection a string representing the connection
      */
-    public void updateUIMessage(String connection) {
+    private void updateUIMessage(String connection) {
         //Check if user change fragments
         if (destroyed) {
             return;
@@ -282,7 +363,7 @@ public class DashboardFragment extends Fragment {
             case SUCCESS:
                 //Update bluetooth logo
                 bluetoothImageView.setImageResource(R.drawable.ic_bluetooth_connected);
-                bluetoothImageView.setContentDescription("Connected to " + deviceName);
+                bluetoothImageView.setContentDescription("Connected to Guide-Robot");
                 //Update message
                 feedbackTextView.setVisibility(View.VISIBLE);
                 feedbackTextView.setText(R.string.connection_success);
@@ -302,6 +383,15 @@ public class DashboardFragment extends Fragment {
                 feedbackTextView.setVisibility(View.VISIBLE);
                 feedbackTextView.setText(R.string.command_failed);
                 break;
+        }
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        destroyed = true;
+        if (myConnection != null) {
+            myConnection.closeSocket();
         }
     }
 }//EOF Class
